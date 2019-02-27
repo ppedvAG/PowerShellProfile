29fd017db5651e4906138c07e6493b2576f758ef
Stefan Ober <ppedvStefanO@users.noreply.github.com>
2019-02-27 17:00:15 +0100

    Update auf PSReadlineOption V2

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,26 @@ $ShellSitzung =  $Host.UI.RawUI
 $ShellSitzung.WindowTitle= "StefanO Shell"
 $ShellSitzung.BackgroundColor = "White"
 $ShellSitzung.ForegroundColor = "Black"
-
+if((Get-Command Set-PSReadLineOption).Version.Major -lt 2)
+{
+Write-Host "erster Teil"
 Set-PSReadlineOption -TokenKind Command   -ForegroundColor DarkBlue
 Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Blue
 Set-PSReadlineOption -TokenKind Number    -ForegroundColor DarkRed
 Set-PSReadlineOption -TokenKind Member    -ForegroundColor Gray
-
+}
+else
+{
+Write-Host "zweiter Teil"
+Set-PSReadLineOption -Colors @{"Parameter" = [ConsoleColor]::Blue
+                               "Command"   = [Consolecolor]::DarkBlue
+                               "Number"    = [Consolecolor]::DarkRed
+                               "Member"    = [ConsoleColor]::Gray
+                               "String"    = [Consolecolor]::DarkGreen
+                               "Comment"   = [ConsoleColor]::Green
+                               "Keyword"   = [ConsoleColor]::Magenta
+                               "None"      = [ConsoleColor]::Black
+                               "Operator"  = [ConsoleColor]::DarkMagenta
+                               "Type"      = [ConsoleColor]::Cyan
+                               "Variable"  = [ConsoleColor]::DarkCyan}
+}
